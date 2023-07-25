4215909653c39aaf75fbc8a549e61737fd25fa30
Kirill_Kochugin <kraingor@yandex.ru>
2023-07-25 14:40:30 +0300

    user inputed array added

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿string [] ArrayInput (int resultArrayLength)
+{
+    string[] resultArray = new string[resultArrayLength];
+    for(int index = 0; index < resultArrayLength; index++)
+    {
+        Console.WriteLine($"Введите {index + 1} эллемент массива");
+        resultArray[index] = Console.ReadLine()!;
+    }
+    return resultArray;
+}
+
+void PrintArray(string[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        System.Console.Write($"< {Array[i]} > ");
+    }
+}
+
+int CheckInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string InputStr = Console.ReadLine()!;
+    int value;
+    if (int.TryParse(InputStr, out value))
+    {
+        return value;
+    }
+    Console.WriteLine("Извините, но вы ввели не целое число");
+    Environment.Exit(0);
+    return 0;
+}
+
+
+int inputArrayLength = CheckInt("Введите число элементов массива");
+string [] inputArray = ArrayInput(inputArrayLength);
+Console.WriteLine("Все элементы введенного вами массива (< element >): ");
+PrintArray(inputArray);
